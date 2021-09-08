1e0d76f381e323da57ab62e0c010f36aad9cee07
Mahak Agrawal <87558858+itsMahakA@users.noreply.github.com>
2021-09-09 00:20:10 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,28 @@
+// Input Format
+// A number n
+
+// Output:
+// *	
+// *	*	
+// *	*	*	
+// *	*	*	*	
+// *	*	*	*	*	
+
+import java.util.*;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        // write ur code here
+        for(int i = 1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print("*\t");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
