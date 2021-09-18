3bec9aabce5f8347060d8c89f5148156d57dc62c
Mahak Agrawal <87558858+itsMahakA@users.noreply.github.com>
2021-09-18 15:25:06 +0530

    Update AnyBaseToAnyBase.java

--- Level_1/Functions/AnyBaseToAnyBase.java
+++ Level_1/Functions/AnyBaseToAnyBase.java
@@ -17,17 +17,40 @@ public class AnyBaseToAnyBase{
     }
 
     public static int getResult(int n ,int b1,int b2){
-        int result =0;
-        int mul =1;
-        while(n>0){
-            int quo = n/b2;
-            int rem = n%b2;
+        int part1 = b1ToDecimal(n,b1);
+        int part2 = decimalTob2(part1 , b2);
 
+        return part2;
+    }
+
+    public static int b1ToDecimal(int n ,int b1){
+        int result = 0;
+        int mul = 1;
+        while(n>0){
+            int quo = n/10;
+            int rem = n%10;
+        
             result+=(rem*mul);
-            mul = mul*b1;
+            mul = mul * b1;
             n = quo;
+      }
+      return result;
+    }
+
+    public static int decimalTob2(int num , int b2){
+        int mul = 1;
+        int result =0 ;
+     
+        while(num>0){
+         int quo = num/b2;
+         int rem = num%b2;
+         result+=(rem*mul);
+         num = quo;
+         mul = mul*10;
+ 
         }
         return result;
     }
 }
+
 
