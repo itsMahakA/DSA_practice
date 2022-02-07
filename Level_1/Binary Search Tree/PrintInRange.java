0c827f77347e4f26b3df5ae8686e3c42e142d9c4
Mahak Agrawal <87558858+itsMahakA@users.noreply.github.com>
2022-02-07 23:02:43 +0530

    Create PrintInRange.java

---
+++ Level_1/Binary Search Tree/PrintInRange.java
@@ -0,0 +1,17 @@
+//https://nados.io/question/print-in-range
+public static void pir(Node node, int d1, int d2) {
+  // write your code here
+  if(node == null) return ;
+
+  if(node.data >= d1 && node.data <= d2){
+    //both calls
+    pir(node.left,d1,d2);
+    System.out.println(node.data);
+    pir(node.right,d1,d2);
+  }
+  else if(node.data > d1 && node.data > d2){
+    pir(node.left,d1,d2);
+  }else if(node.data < d1 && node.data < d2){
+    pir(node.right,d1,d2);
+  }
+}
