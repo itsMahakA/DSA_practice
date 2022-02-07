b795fe53599bac254da93ba4da83954cbc5554ea
Mahak Agrawal <87558858+itsMahakA@users.noreply.github.com>
2022-02-07 23:13:01 +0530

    Create LCA.java

---
+++ Level_1/Binary Search Tree/LCA.java
@@ -0,0 +1,15 @@
+//https://nados.io/question/lca-of-bst
+public static int lca(Node node, int d1, int d2) {
+  // write your code here
+  if(node == null) return 0;
+
+  if(node.data > d1 && node.data > d2){
+    return lca(node.left,d1,d2);
+  }else if(node.data < d1 && node.data < d2){
+    return lca(node.right, d1,d2);
+  }
+  else{
+    return node.data;
+  }
+
+}
